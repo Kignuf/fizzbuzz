7a098c5987e5d5bda38445b2bb25e9dbdf50c638
Kevin Roulleau <kevin.roulleau@happn.com>
2018-09-03 14:55:38 +0200

    force test to fail

--- test/fizzbuzz.js
+++ test/fizzbuzz.js
@@ -3,7 +3,8 @@ const FizzBuzz = require('../src/fizzbuzz')
 
 describe('FizzBuzz', function () {
 	it('Prints Fizz for numbers divisible by 2', function () {
-		expect(FizzBuzz.getString(2)).to.equal('Fizz')
+		throw new Error('toto')
+		// expect(FizzBuzz.getString(2)).to.equal('Fizz')
 	})
 	it('Prints Buzz for numbers divisible by 3', function () {
 		expect(FizzBuzz.getString(3)).to.equal('Buzz')
